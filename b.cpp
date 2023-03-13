1112707c84413fe5e709264beb9bc0a763bdaee2
Pham Duc Manh <127580035+phongnhale1709@users.noreply.github.com>
2023-03-13 15:26:41 +0700

    Update b.cpp

--- b.cpp
+++ b.cpp
@@ -3,9 +3,7 @@ using namespace std;
 int main()
 {
 float a,b;
-cout <<"nhap gia tri cua so a la ";
 cin >>a;
-cout <<"nhap gia tri cua so b la ";
 cin >>b;
 cout <<"tong la "<< a+b<<endl;
 cout <<"tich la "<< a*b<<endl;
